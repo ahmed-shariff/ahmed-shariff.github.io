a86f99611bfda0847f98ced28fe90ddefa239574
Ahmed Shariff <shariff.mfa@outlook.com>
2022-04-16 03:24:52 -0700

    Working next app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* const withPlugins = require('next-compose-plugins');
+* const optimizedImages = require('next-optimized-images');
+* /* const nextImages = require('next-images') */
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+// const nextConfig = withPlugins([
+//     [optimizedImages, {
+//         mozjpeg: {
+//             quality: 80,
+//         },
+//         pngquant: {
+//             speed: 3,
+//             strip: true,
+//             verbose: true,
+//         },
+//         imagesPublicPath: '/_next/static/images/',
+//     }],
+//     {
+//         /* The following is needed with subdomains 
+//          * basePath: '/<path>',
+//          * assetPrefix: '/<path>/', */
+//         reactStrictMode: true,
+//     },
+// ]);
+
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        loader: "custom",
+    }
+};
+
+module.exports = nextConfig;
