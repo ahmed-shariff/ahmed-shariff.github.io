e0c80641e6eba9af16f3d94cfad86273caf17d67
Ahmed Shariff <shariff.mfa@outlook.com>
2023-12-03 23:30:57 -0800

    Fix issues styling & images in posts

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -2,7 +2,7 @@
  export let data;
 </script>
 
-<article>
+<article class="p-5 md:p-0 prose dark:prose-invert text-justify mx-auto max-w-screen-xl prose-img:block prose-img:m-auto prose-img:max-h-96 prose-p:w-full">
     <h1>
         {#if data.published}
             [DRAFT]
