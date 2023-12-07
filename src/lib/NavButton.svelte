aa335e51dcda2a24fb29b97783aaac8d81cc1583
Ahmed Shariff <shariff.mfa@outlook.com>
2023-12-06 20:19:12 -0800

    NavButton force reload page

--- src/lib/NavButton.svelte
+++ src/lib/NavButton.svelte
@@ -2,6 +2,6 @@
  export let href;
 </script>
 
-<a class="transition duration-100 bg-transparent shadow-md shadow-transparent rounded-lg hover:shadow-gray-900 hover:bg-gray-700 hover:underline hover:decoration-2 px-4 py-2 items-center" href="{href}">
+<a class="transition duration-100 bg-transparent shadow-md shadow-transparent rounded-lg hover:shadow-gray-900 hover:bg-gray-700 hover:underline hover:decoration-2 px-4 py-2 items-center" href="{href}" data-sveltekit-reload>
     <slot/>
 </a>
